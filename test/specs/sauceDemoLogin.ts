0c7fe84a533451dea381194a2fd2edcb91e92eec
testsadab <testsadab@gmail.com>
2023-01-04 17:25:36 +1100

    interacti element

--- test/specs/sauceDemoLogin.ts
+++ test/specs/sauceDemoLogin.ts
@@ -1,9 +1,11 @@
 describe("Sauce demo app login", ()=> {
 
-    it("should login with valid standard user",  async ()=> {
-
+    beforeEach("Open App", async ()=> {
         await browser.url("https://www.saucedemo.com/");
+        await browser.maximizeWindow();
+    })
 
+    it("should login with valid standard user",  async ()=> {
         const userNameTextBox = $("#user-name");
         const passwordTextBox = $("#password");
         const loginButton = $("#login-button");
@@ -15,5 +17,29 @@ describe("Sauce demo app login", ()=> {
         const productTitle = $(".title");
         await expect(productTitle).toBeDisplayed();
     })
+
+    it("should login with valid standard user",  async ()=> {
+        const userNameTextBox = $("#user-name");
+        await userNameTextBox.setValue("standard_user");
+        console.log("User Name with set value: ", await userNameTextBox.getValue());
+
+        await userNameTextBox.addValue("OneMoreUser");
+        console.log("User Name with add value: ", await userNameTextBox.getValue());
+
+        await userNameTextBox.clearValue();
+        console.log("User Name with clear value: ", await userNameTextBox.getValue());
+    })
+
+    it('should fetch element text', async () => {
+        const userHeader = $("#login_credentials >h4");
+        const headerText = await userHeader.getText();
+        console.log(headerText);
+        await expect(userHeader).toHaveText("Accepted usernames are:")
+        
+        const loginAreaElement = $("#login_credentials");
+        const loginClassValue = await loginAreaElement.getAttribute("class");
+        console.log(loginClassValue);
+        await expect(loginAreaElement).toHaveAttribute("class", "login_credentials");
+    });
 
 })
