1adb45d4ece165995c4b87cd97aec534c1031dbe
testsadab <testsadab@gmail.com>
2023-01-06 00:32:15 +1100

    updated failed test

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,5 +1,5 @@
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
+import LoginPage from '../pageobjects/login.page'
+import SecurePage from '../pageobjects/secure.page'
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -31,7 +31,7 @@ export const config: Options.Testrunner = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.ts'
+        './test/specs/**/example.e2e.ts'
     ],
     // Patterns to exclude.
     exclude: [
