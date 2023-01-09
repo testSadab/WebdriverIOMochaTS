a9ca7df18fc3ea8e92c01eb4664f04577a33ee7a
testsadab <testsadab@gmail.com>
2023-01-09 15:38:59 +1100

    added wait demo test

---
+++ test/pageobjects/wait.page.ts
@@ -0,0 +1,16 @@
+class WaitPage {
+
+    private get startButton() { return $("#start>button") }
+    get helloWorldHeader() {  return $("#finish>h4")}
+    get loadingIcon() {return $("#loading")}
+
+    async clickStartButton() {
+        await this.startButton.click();
+    }
+
+    async waitForLoadingIconToDisappear() {
+        await this.loadingIcon.waitForDisplayed();
+        await this.loadingIcon.waitForDisplayed({reverse: true, timeout: 10000, timeoutMsg: "Failed while waiting for loading icon to disappear"});
+    }
+}
+export default new WaitPage();
---
+++ test/specs/wait.example.ts
@@ -0,0 +1,24 @@
+import waitPage from "../pageobjects/wait.page.js";
+
+describe('feature: wait strategy', () => {
+
+    beforeEach("open app", async ()=> {
+        await browser.url("https://the-internet.herokuapp.com/dynamic_loading/1");
+        await browser.maximizeWindow();
+    })
+
+    it('should wait for element to appear', async () => {
+        await waitPage.clickStartButton();
+
+        // await waitPage.helloWorldHeader.waitForDisplayed({timeout: 10000, timeoutMsg: "failed for header to appear"})
+        await browser.waitUntil(async () => await waitPage.helloWorldHeader.getText() === "Hello World!", { timeout: 10000, timeoutMsg: "failed for text header to match" })
+        await expect(waitPage.helloWorldHeader).toHaveText("Hello World!");
+
+    });
+
+    it.only("should wait for element to disappear", async ()=> {
+        await waitPage.clickStartButton();
+        await waitPage.waitForLoadingIconToDisappear();
+        await expect(waitPage.helloWorldHeader).toHaveText("Hello World!");
+    })
+});
