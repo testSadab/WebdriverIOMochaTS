646abb3f3185bc14e103be66b855af733f57d2e1
testsadab <testsadab@gmail.com>
2023-01-09 15:40:48 +1100

    added more wait example

--- test/specs/wait.example.ts
+++ test/specs/wait.example.ts
@@ -9,11 +9,14 @@ describe('feature: wait strategy', () => {
 
     it('should wait for element to appear', async () => {
         await waitPage.clickStartButton();
+        await waitPage.helloWorldHeader.waitForDisplayed({timeout: 10000, timeoutMsg: "failed for header to appear"})
+        await expect(waitPage.helloWorldHeader).toHaveText("Hello World!");
+    });
 
-        // await waitPage.helloWorldHeader.waitForDisplayed({timeout: 10000, timeoutMsg: "failed for header to appear"})
+    it('should wait until element to appear', async () => {
+        await waitPage.clickStartButton();
         await browser.waitUntil(async () => await waitPage.helloWorldHeader.getText() === "Hello World!", { timeout: 10000, timeoutMsg: "failed for text header to match" })
         await expect(waitPage.helloWorldHeader).toHaveText("Hello World!");
-
     });
 
     it.only("should wait for element to disappear", async ()=> {
