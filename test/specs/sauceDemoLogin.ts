d5c8a10ab3fe7295e0904e389063ff338464db0d
testsadab <testsadab@gmail.com>
2023-01-10 15:14:21 +1100

    json data

--- test/specs/sauceDemoLogin.ts
+++ test/specs/sauceDemoLogin.ts
@@ -1,19 +1,24 @@
 import sauceLoginPage from "../pageobjects/sauce.login.page.js";
+import logindata from "../data/login.json" assert {type: "json"}
 
-describe("Sauce demo app login", ()=> {
+describe("Sauce demo app login", () => {
 
-    beforeEach("Open App", async ()=> {
+    beforeEach("Open App", async () => {
         await sauceLoginPage.openApp();
     })
 
-    it("should login with valid standard user",  async ()=> {
-        await sauceLoginPage.performLogin("standard_user", "secret_sauce")
-
+    it("should login with valid standard user", async () => {
+        await sauceLoginPage.performLogin(logindata.valid.username, logindata.valid.password);
         await expect(sauceLoginPage.productTitle).toBeDisplayed();
     })
 
-    it("should login with valid standard user",  async ()=> {
-        await sauceLoginPage.userNameTextBox.setValue("standard_user");
+    it("should not login with invalid user", async () => {
+        await sauceLoginPage.performLogin(logindata.invalid.username, logindata.invalid.password);
+        await expect(sauceLoginPage.productTitle).not.toBeDisplayed();
+    })
+
+    it("should login with valid standard user", async () => {
+        await sauceLoginPage.userNameTextBox.setValue(logindata.valid.username);
         console.log("User Name with set value: ", await sauceLoginPage.userNameTextBox.getValue());
 
         await sauceLoginPage.userNameTextBox.addValue("OneMoreUser");
@@ -27,7 +32,7 @@ describe("Sauce demo app login", ()=> {
         const headerText = await sauceLoginPage.userHeader.getText();
         console.log(headerText);
         await expect(sauceLoginPage.userHeader).toHaveText("Accepted usernames are:")
-        
+
         const loginClassValue = await sauceLoginPage.loginAreaElement.getAttribute("class");
         console.log(loginClassValue);
         await expect(sauceLoginPage.loginAreaElement).toHaveAttribute("class", "login_credentials");
