444011e2e39d070192d878df2d18e940e7c43bea
testsadab <testsadab@gmail.com>
2023-01-11 20:42:16 +1100

    dropdown tests

---
+++ test/pageobjects/dropdown.page.ts
@@ -0,0 +1,18 @@
+class DropdownPage {
+
+    get skillsDropdown() { return $("#Skills") };
+
+    private get countryContainer() { return $("//span[@aria-labelledby='select2-country-container']") }
+    private get searchInput() { return $("//input[@type='search']") }
+    private get passwordInput() { return $("#firstpassword") }
+
+    async selectCountry(countryName:string) {
+        const countryElement = $(`//*[@id='select2-country-results']/li[text()='${countryName}']`);
+        await this.passwordInput.scrollIntoView();
+        await this.countryContainer.click();
+        await this.searchInput.setValue(countryName);
+        await countryElement.click();
+    }
+
+}
+export default new DropdownPage();
---
+++ test/specs/dropdown.ts
@@ -0,0 +1,25 @@
+import dropdownPage from "../pageobjects/dropdown.page.js";
+
+describe('feature: handle dropdown', () => {
+
+    beforeEach("Open App", async() => {
+        await browser.url("https://demo.automationtesting.in/Register.html");
+        await browser.maximizeWindow();
+    })
+    
+    it('should select dropdown by text', async () => {
+        await dropdownPage.skillsDropdown.selectByVisibleText("Adobe InDesign");
+    });
+
+    it('should select dropdown by index', async() => {
+        await dropdownPage.skillsDropdown.selectByIndex(3);
+    });
+
+    it('should select dropdown by attribute', async() => {
+        await dropdownPage.skillsDropdown.selectByAttribute("value", "Android");
+    });
+
+    it.only("should select dropdown value without select tag", async()=> {
+        await dropdownPage.selectCountry("India");
+    })
+});
