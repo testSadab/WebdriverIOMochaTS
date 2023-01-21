e1e954ce59fbac3739274031028eb26c327197e9
testsadab <testsadab@gmail.com>
2023-01-21 11:39:29 +1100

    handle picker date

--- test/pageobjects/calendar.page.ts
+++ test/pageobjects/calendar.page.ts
@@ -1,17 +1,22 @@
 class CalendarPage {
 
-    private get simpleDatePickerTab() {  return $("//*[@id='Simple Date Picker']")}
+    private get simpleDatePickerTab() { return $("//*[@id='Simple Date Picker']") }
+    get iframeSimpleDate() { return $("//iframe[contains(@data-src, 'datepicker/default')]") }
+    get datePickerInput() { return $("#datepicker") }
 
-    get iframeSimpleDate() {return $("//iframe[contains(@data-src, 'datepicker/default')]")}
+    private get dropdownDatePickerTab() { return $("//*[@id='DropDown DatePicker']") }
+    get iframeDropdownDate() { return $("//iframe[contains(@data-src, 'datepicker/dropdown-month-year')]") }
+    private get selectMonth() { return $("[data-handler=selectMonth]") }
+    private get selectYear() { return $("[data-handler=selectYear]") }
+    private selectDay = (day: number) => $(`//*[@id='ui-datepicker-div']//table//td[@data-handler='selectDay']/a[text()='${day}']`);
 
-    get datePickerInput() {return $("#datepicker")}
-
-
-    private get dropdownDatePickerTab()  { return $("//*[@id='DropDown DatePicker']")}
-    get iframeDropdownDate() {return $("//iframe[contains(@data-src, 'datepicker/dropdown-month-year')]")}
-    private get selectMonth() {return $("[data-handler=selectMonth]")}
-    private get selectYear() { return $("[data-handler=selectYear]")}
-    private selectDay = (day: number) =>  $(`//*[@id='ui-datepicker-div']//table//td[@data-handler='selectDay']/a[text()='${day}']`);
+    private get iconTriggerDatePickerTab() { return $("//*[@id='Icon Trigger']") }
+    get iframeTriggerDate() { return $("//iframe[contains(@data-src,'icon-trigger')]") }
+    private get datePickerCalendarIcon() { return $(".ui-datepicker-trigger") }
+    private get monthDatePicker() { return $(".ui-datepicker-month") }
+    private get yearDatePicker() { return $(".ui-datepicker-year") }
+    private get prevIcon() { return $("//a[@title='Prev']") }
+    private get nextIcon() { return $("//a[@title='Next']") }
 
     async clickOnSimpleDatePickerTab() {
         await this.simpleDatePickerTab.click();
@@ -21,11 +26,48 @@ class CalendarPage {
         await this.dropdownDatePickerTab.click();
     }
 
-    async selectDropdownDate(day: number, month:string, year: number) {
+    async clickOnIconTriggerDatePickerTab() {
+        await this.iconTriggerDatePickerTab.click();
+    }
+
+    async selectDropdownDate(day: number, month: string, year: number) {
         await this.selectMonth.selectByVisibleText(month);
         await this.selectYear.selectByVisibleText(year);
         await this.selectDay(day).click()
     }
 
+    private async getActualDateValue() {
+        const actualMonth = await this.monthDatePicker.getText();
+        const actualYear = await this.yearDatePicker.getText();
+        const actualMonthInNumber = new Date(`${actualMonth}, 2012`).getMonth() + 1;
+        const actualYearInNumber = Number(actualYear);
+        const actualDateValue = new Date(actualYearInNumber, actualMonthInNumber).valueOf();
+        return actualDateValue;
+    }
+
+    private getExpectedDateValue(month: string, year: number) {
+        const expectedMonthInNumber = new Date(`${month}, 2012`).getMonth() + 1;
+        const expectedDateValue = new Date(year, expectedMonthInNumber).valueOf();
+        return expectedDateValue;
+    }
+
+    private async navigateToMatchingDateGrid(month: string, year: number) {
+        while (await this.getActualDateValue() !== this.getExpectedDateValue(month, year)) {
+            if (await this.getActualDateValue() > this.getExpectedDateValue(month, year)) {
+                await this.prevIcon.waitForClickable();
+                await this.prevIcon.click();
+            } else if (await this.getActualDateValue() < this.getExpectedDateValue(month, year)) {
+                await this.nextIcon.waitForClickable();
+                await this.nextIcon.click();
+            }
+        }
+    }
+
+    async selectDateFromIconTrigger(day: number, month: string, year: number) {
+        await this.datePickerCalendarIcon.waitForClickable();
+        await this.datePickerCalendarIcon.click();
+        await this.navigateToMatchingDateGrid(month, year)
+        await this.selectDay(day).click();
+    }
 }
 export default new CalendarPage();
--- test/specs/calendar.ts
+++ test/specs/calendar.ts
@@ -7,7 +7,7 @@ describe('feature: handle calendar', () => {
         await browser.maximizeWindow();
     })
 
-    it.skip('should handle simple date picker', async () => {
+    it('should handle simple date picker', async () => {
         await calendarPage.clickOnSimpleDatePickerTab();
         await browser.switchToFrame(await calendarPage.iframeSimpleDate)
         await calendarPage.datePickerInput.setValue("01/19/2023");
@@ -22,6 +22,16 @@ describe('feature: handle calendar', () => {
         await browser.switchToFrame(await calendarPage.iframeDropdownDate);
         await calendarPage.datePickerInput.click();
         await calendarPage.selectDropdownDate(day, month, year);
+        await browser.switchToParentFrame();
+    });
+
+    it.only('should handle icon picker date', async () => {
+        await calendarPage.clickOnIconTriggerDatePickerTab();
+
+        const day=20,  month = "Dec", year = 2021;
+
+        await browser.switchToFrame(await calendarPage.iframeTriggerDate);
+        await calendarPage.selectDateFromIconTrigger(day, month, year);
         await browser.switchToParentFrame();
     });
 });
